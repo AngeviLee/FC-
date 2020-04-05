91d85622cca51b90a4c69c0f3b289eaee14d8811
AngeviLee <568178364@qq.com>
2020-04-05 16:41:12 +0800

    小程序的tap、focus、blur事件及事件绑定原则

--- fc游乐园/pages/center/index.js
+++ fc游乐园/pages/center/index.js
@@ -1,66 +1,15 @@
-// pages/center/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data: {},
+  
+  sayHi: function() {
+    console.log("我被点击了")
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  in() {
+    console.log("input的focus")
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  out() {
+    console.log("input的blur")
   }
 })
