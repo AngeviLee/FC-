b4347f3959aa5b4a6b2cad6b1ee455caf4135315
AngeviLee <568178364@qq.com>
2020-04-07 22:50:49 +0800

    小程序表单-单选、复选-获取数据

--- fc游乐园/pages/form/index.js
+++ fc游乐园/pages/form/index.js
@@ -1,7 +1,30 @@
 Page({
+  data: {
+    gender: '男',
+    hobby: []
+  },
 
   handleSubmit(ev) {
     console.log('我被提交了')
+    console.log("gender", this.data.gender)
+    console.log("hobby", this.data.hobby)
     console.log(ev)
+  },
+
+  getRadioValue(ev) {
+    console.log("单选框被点击了", ev)
+    this.setData({
+      gender: ev.detail.value
+    })
+    console.log("this.data.gender", this.data.gender)
+  },
+
+  getCheckboxValue(ev) {
+    console.log("能被触发")
+    console.log("复选框ev",ev)
+    this.setData({
+      hobby: ev.detail.value
+    })
+    console.log("this.data.hobby", this.data.hobby)
   }
 })
